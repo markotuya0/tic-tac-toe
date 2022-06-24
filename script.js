6a76d9f8e41d765625371703fbd001a634190d3b
Mark Otuya <markotuya0@gmail.com>
2022-06-24 17:53:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
 const restartButton = document.getElementById('restartButton')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
