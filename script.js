7c671fa3526ff58b87508f883daadfbf9ed90e29
Mark Otuya <markotuya0@gmail.com>
2022-06-24 17:50:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
